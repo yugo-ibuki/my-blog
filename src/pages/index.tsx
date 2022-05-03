60b96a9b3c9f60586cc0b652a86b8e2ef10eb2a2
yugo-ibuki <y.ibuki91@gmail.com>
2022-05-04 00:41:47 +0900

    feat: 検索ボックス追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,30 @@ import type { GetStaticProps, NextPage } from 'next'
 import { client } from '../libs/client'
 import { MicroCMSListResponse } from 'microcms-js-sdk/dist/cjs/types'
 import { Blog, Post } from '@components/Posts'
-import { Box, Grid } from '@mantine/core'
+import { Box, Button, Grid, Input } from '@mantine/core'
 
 type Props = MicroCMSListResponse<Blog>
 
 const Index: NextPage<Props> = (props) => {
   return (
     <Box className={'mt-10'}>
+      <Box mb={20}>
+        <Grid>
+          <Grid.Col className={'max-w-[80%]'}>
+            <Input />
+          </Grid.Col>
+          <Grid.Col className={'max-w-[20%]'}>
+            <Button
+              variant="gradient"
+              color={'dark'}
+              fullWidth
+              className={"text-cyan-300 border-solid border-cyan-200 border-[1px] rounded hover:border-cyan-500 hover:text-cyan-500"}
+            >
+              Search
+            </Button>
+          </Grid.Col>
+        </Grid>
+      </Box>
       <Grid className={'gap-y-8'}>
         {
           props.contents.map(blog =>
