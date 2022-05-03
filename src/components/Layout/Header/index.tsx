f07b0e708882234fde952bd6fe96c1bd01eff33a
yugo-ibuki <y.ibuki91@gmail.com>
2022-05-03 15:27:54 +0900

    fix: headerレイアウト修正

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -5,16 +5,14 @@ import { SiMicrodotblog } from 'react-icons/si'
 export const Header: React.FC = () => {
   return (
     <header>
-      <div className={'px-[10px]'}>
-        <div className={'w-[700px] sp:w-full py-[15px] c-flex mx-auto'}>
-          <div className={'flex gap-x-3 w-[500px]'}>
-            <Link href="/">
-              <a className={'flex items-center gap-x-3'}>
-                <SiMicrodotblog />
-                <h1 className={'font-bold text-lg mr-2'}>Blog</h1>
-              </a>
-            </Link>
-          </div>
+      <div className={'w-[700px] sp:w-full py-[15px] px-5 mx-auto'}>
+        <div className={'flex gap-x-3 w-[500px]'}>
+          <Link href="/">
+            <a className={'flex items-center gap-x-3'}>
+              <SiMicrodotblog />
+              <h1 className={'font-bold text-lg mr-2'}>Blog</h1>
+            </a>
+          </Link>
         </div>
       </div>
     </header>
