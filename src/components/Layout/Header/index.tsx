1463854b66922669b1836886727d73ce5b3df2c1
yugo-ibuki <y.ibuki91@gmail.com>
2022-05-03 10:32:34 +0900

    fix: ヘッダーアイコン周り修正

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
+import { SiMicrodotblog } from 'react-icons/si'
 
 export const Header: React.FC = () => {
   return (
     <header>
       <div className={'px-[10px]'}>
         <div className={'w-[700px] sp:w-full py-[15px] c-flex mx-auto'}>
-          <div className={'c-flex gap-x-3 w-full'}>
+          <div className={'flex gap-x-3 w-[500px]'}>
             <Link href="/">
-              <a className={'c-flex gap-x-3'}>
+              <a className={'flex items-center gap-x-3'}>
+                <SiMicrodotblog />
                 <h1 className={'font-bold text-lg mr-2'}>Blog</h1>
               </a>
             </Link>
